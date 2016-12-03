0256ce117cd17de56ac9393113d829e33fa13145
Rachanee Saengkrajai <rachanee.saeng@gmail.com>
2016-12-03 10:56:01 +0700

    Add review

--- script/app.js
+++ script/app.js
@@ -1,8 +1,29 @@
 (function(){
   var app = angular.module('store',[ ]);
 
-  app.controller('StoreCtrl', function(){
+  app.controller('StoreController', function(){
     this.products = prods;
+  });
+
+  app.controller('PanelController', function(){
+    this.tab = 1;
+    this.selectTab = function(selectedTab){
+      this.tab = selectedTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('ReviewController', function(){
+    this.detail = {};
+    this.isInputing = function(){
+      return !angular.equals(this.detail, {});
+    }
+    this.addReview = function(product){
+      product.reviews.push(this.detail);
+      this.detail = {};
+    };
   });
 
   var prods = [
@@ -17,7 +38,8 @@
           full: 'img/Note5_xl.jpg',
           thum: 'img/Note5_s.jpg'
         }
-      ]
+      ],
+      reviews: []
     },
     {
       name: 'Samsung Galaxy S7',
@@ -30,7 +52,8 @@
           full: 'img/s7_xl.jpg',
           thum: 'img/s7_s.jpg'
         }
-      ]
+      ],
+      reviews: []
     },
     {
       name: 'Samsung Galaxy J7 Prime',
@@ -42,6 +65,13 @@
         {
           full: 'img/j7Prime_xl.jpg',
           thum: 'img/j7Prime_s.jpg'
+        }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "so good",
+          author: "rachane."
         }
       ]
     }
